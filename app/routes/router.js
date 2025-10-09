4f92d4455a2ee48d645dde95c29fa68bd44b7d64
SalesOR <rm95030@estudante.fieb.edu.br>
2025-10-08 21:59:11 -0300

    coisa

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,19 +1,12 @@
 var express = require('express');
 const { body, validationResult } = require('express-validator');
 var router = express.Router();
-var { valCPF, valTel } = require('../helpers/validacoes');
+var { valCPF, valTel, valNasc } = require('../helpers/validacoes');
 
 router.get('/', function(req, res) {
     res.render('pages/cadastro', { 
         "erros": null, "valores": {"nome": "", "nasc": "", "cpf": "", "tel": "", "email": "", "senhan": "", "csenha": ""},"listaErros": null});});
 
-router.get('/login', function(req, res) {
-    res.render('pages/login');
-});
-
-router.get('/home', function(req, res) {
-    res.render('pages/home');
-});
 
 router.post("/cadastro",
     body("nome")
@@ -25,6 +18,17 @@ router.post("/cadastro",
                 return true;
             } else {
                 throw new Error('CPF inválido!');
+            }
+        }),
+    // **NOVA VALIDAÇÃO DE DATA DE NASCIMENTO (NASC)**
+    body("nasc")
+        // Garante que o formato é AAAA-MM-DD
+        .isISO8601().withMessage('Data de Nascimento deve ser uma data válida no formato AAAA-MM-DD!')
+        .custom((value) => {
+            if (valNasc(value)) {
+                return true;
+            } else {
+                throw new Error('Data de Nascimento inválida! A idade deve ser no máximo 110 anos e não pode ser uma data futura.');
             }
         }),
     body("tel")
@@ -41,7 +45,7 @@ router.post("/cadastro",
     body("senhan")
         .isLength({ min: 6, max: 20 }).withMessage('A senha deve conter de 6 a 20 caracteres!')
         .custom((value) => {
-            const regex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{$/;
+            const regex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{6,20}$/; // Corrigi a regex
             if (!regex.test(value)) {
                 throw new Error('A senha deve conter pelo menos um número, uma letra maiúscula e um caractere especial!');
             }
@@ -72,7 +76,7 @@ router.post("/cadastro",
         });
     }
 );
-;
+
 router.get('/login', function(req, res) {
     res.render('pages/login');
 });
@@ -81,6 +85,12 @@ router.get('/home', function(req, res) {
 });
 router.get('/usuario', function(req, res) {
     res.render('pages/usuario');
+});
+router.get('/bloco', function(req, res) {
+    res.render('partial/bloco');
+});
+router.get('/grid', function(req, res) {
+    res.render('partial/grid');
 });
 
 module.exports = router;
